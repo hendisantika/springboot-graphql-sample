eb7a12e9fefa15c1912c81336d2ac3b2474ee7fe
Hendi Santika <hendisantika@gmail.com>
2020-01-26 06:11:16 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootgraphqlsample/SpringbootGraphqlSampleApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootgraphqlsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGraphqlSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootGraphqlSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springbootgraphqlsample/SpringbootGraphqlSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootgraphqlsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGraphqlSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
