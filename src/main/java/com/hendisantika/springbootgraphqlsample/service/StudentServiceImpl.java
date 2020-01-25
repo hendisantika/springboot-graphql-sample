d2daa26165ec8f442dda5c6fb982b80792d582e9
Hendi Santika <hendisantika@gmail.com>
2020-01-26 06:16:24 +0700

    #12 - Add StudentServiceImpl

---
+++ src/main/java/com/hendisantika/springbootgraphqlsample/service/StudentServiceImpl.java
@@ -0,0 +1,21 @@
+package com.hendisantika.springbootgraphqlsample.service;
+
+import com.hendisantika.springbootgraphqlsample.model.Student;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-graphql-sample
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 26/01/20
+ * Time: 06.16
+ */
+@Service
+public class StudentServiceImpl implements StudentService {
+    private Map<String, Student> map = new HashMap<>();
+}
